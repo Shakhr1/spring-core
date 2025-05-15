6a90eb40b7fabd021098c9a2ee919471c817130e
Shakh Shukhratov <shakhriplay@gmail.com>
2025-05-15 22:01:14 +0300

    practice

--- src/main/java/school/sorokin/springcore/Main.java
+++ src/main/java/school/sorokin/springcore/Main.java
@@ -8,9 +8,16 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext("school.sorokin.springcore");
+
         Task task = context.getBean(Task.class);
         System.out.println(task);
+        System.out.println("------------------------------");
+
         TaskManager taskManager = context.getBean(TaskManager.class);
         taskManager.print();
+        System.out.println("------------------------------");
+
+        Task task2 = (Task) context.getBean("task");
+        System.out.println(task2);
     }
 }
